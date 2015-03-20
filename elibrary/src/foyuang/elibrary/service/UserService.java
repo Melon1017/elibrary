67cc3647251b02f0c87b50a5a9fe99aac41a5a7d
Melon17 <liuxing1017@outlook.com>
2015-03-20 17:51:00 +0800

    build 0.0.0.2

--- elibrary/src/foyuang/elibrary/service/UserService.java
+++ elibrary/src/foyuang/elibrary/service/UserService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import foyuang.elibrary.entity.User;
 
 public interface UserService {
-	public void regist(User user);
+	public int regist(User user);
 	public boolean userLogin(String userName,String password);
 	public User updateUserProfile(User updatedUser);
 	public User upgradeToVip(User vipUser);
--- elibrary/src/foyuang/elibrary/serviceImpl/UserServiceImpl.java
+++ elibrary/src/foyuang/elibrary/serviceImpl/UserServiceImpl.java
@@ -6,16 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import foyoung.elibrary.common.ErrorCode;
 import foyuang.elibrary.dao.UserDao;
 import foyuang.elibrary.entity.User;
+import foyuang.elibrary.util.DataRegexUtil;
+
 @Service("userService")
 public class UserServiceImpl implements foyuang.elibrary.service.UserService {
 	@Autowired
 	UserDao userDao;
 
-	public void regist(User user) {
+	public int regist(User user) {
 		// TODO Auto-generated method stub
-		
+		String phone = user.getPhone();
+		String password = user.getPhone();
+		if (phone == null || phone.length() > 50 || phone.isEmpty()||!DataRegexUtil.checkPhone(phone)) {
+			return ErrorCode.USER_PHONE_VAILD;
+		}
+		if (password == null || password.length() > 50 || password.isEmpty()) {
+			return ErrorCode.USER_PASSWORD_VAILD;
+		}
+		user.setName(password);
+		userDao.save(user);
+		return ErrorCode.SUCCESS;
 	}
 
 	public boolean userLogin(String userName, String password) {
